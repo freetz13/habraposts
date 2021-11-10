74dfa9f7305739b1d1b16f2675b799bad9fd5a17
freetz13 <alexander.astafiev.1989@gmail.com>
2021-11-10 22:27:34 +1000

    Add default autofield

--- habrakeeper/settings.py
+++ habrakeeper/settings.py
@@ -121,3 +121,8 @@ DATETIME_FORMAT = 'c'
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
