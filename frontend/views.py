9d01fffd63b0278e533c750473e9b14250cb5df3
Alexander Astafiev <alexander.astafiev.1989@gmail.com>
2023-03-09 22:53:06 +1000

    Add basic pagination

--- frontend/views.py
+++ frontend/views.py
@@ -1,6 +1,15 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+
 from api.models import Article
 
+
 def index(request):
-    articles = Article.objects.all()[:50]
-    return render(request, "frontend/index.html", {"articles": articles})
+    articles = Article.objects.all()
+    paginator = Paginator(articles, 25)  # Show 25 articles per page.
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    return render(
+        request, "frontend/index.html", {"page_obj": page_obj}
+    )
