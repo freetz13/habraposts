8e4247bb3cd481220db138d169cab611c77af42f
Alexander Astafiev <alexander.astafiev.1989@gmail.com>
2023-03-30 23:34:51 +1000

    Add tests for tags

--- api/tests.py
+++ api/tests.py
@@ -38,3 +38,35 @@ class OrphansTestCase(TestCase):
 
         article_2.delete()
         self.assertEqual(UserTag.objects.count(), 0, "There should be no tags")
+
+
+class TagsTestCase(TestCase):
+    def setUp(self):
+        AuthorTag.objects.create(name="python")
+        AuthorTag.objects.create(name="lua")
+
+        UserTag.objects.create(name="databases")
+        UserTag.objects.create(name="programming")
+
+        Article.objects.create(id=1234, title="Writing Python scripts the right way")
+        Article.objects.create(id=4321, title="Writing Python scripts the wrong way")
+
+    def test_correct_author_tag_addition(self):
+        tag_python = AuthorTag.objects.get(name="python")
+        tag_lua = AuthorTag.objects.get(name="lua")
+
+        article = Article.objects.get(title="Writing Python scripts the right way")
+        article.author_tags.add(tag_python)
+        article.author_tags.add(tag_lua)
+
+        self.assertEqual(len(article.author_tags.all()), 2)
+
+    def test_correct_user_tag_addition(self):
+        tag_databases = UserTag.objects.get(name="databases")
+        tag_programming = UserTag.objects.get(name="programming")
+
+        article = Article.objects.get(title="Writing Python scripts the wrong way")
+        article.user_tags.add(tag_databases)
+        article.user_tags.add(tag_programming)
+
+        self.assertEqual(len(article.user_tags.all()), 2)
